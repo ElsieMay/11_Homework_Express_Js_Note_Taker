4781c03d4add6e8d43e592c9838f7ea8f0239b8d
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-08 16:19:13 +1000

    commit for deploy to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const db = require("./db/db.json");
 
-const PORT = process.env.port || 3307;
+const PORT = process.env.port || 3000;
 
 const app = express();
 
